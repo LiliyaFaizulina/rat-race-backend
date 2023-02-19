b8efe54b889c7c65a0fa21dea94efc82a1d18924
LiliyaFaizulina <lily.fed85@gmail.com>
2023-02-19 13:33:51 +0200

    fix bugs

--- controllers/gamer/updateGamer.js
+++ controllers/gamer/updateGamer.js
@@ -3,6 +3,15 @@ const { HttpError } = require("../../helpers");
 
 const updateGamer = async (req, res) => {
   const { _id } = req.gamer;
+  const name = req.body.name;
+
+  if (name) {
+    const gamer = await Gamer.findOne({ name });
+    if (gamer) {
+      throw HttpError(409, "Name in use");
+    }
+  }
+
   const updatedGamer = await Gamer.findByIdAndUpdate(_id, req.body, {
     new: true,
   });
@@ -20,6 +29,7 @@ const updateGamer = async (req, res) => {
       dreamCost: updatedGamer.dreamCost,
       debt: updatedGamer.debt,
       children: updatedGamer.children,
+      note: updatedGamer.note,
     },
   });
 };
